b76d882afe575f70ceceed8801b4405f4bbeedd0
nathanyee <nathaniel.yee@students.olin.edu>
2017-01-22 13:10:50 -0500

    Added second function, getVal, error cards are now val=0

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -7,29 +7,27 @@
 #include <stdlib.h>
 
 int updateCount(int val, int count);
+int getVal(char card_name[]);
 
 int main() {
-  char card_name[3];
+  char card_name[3]; //Need 3 chars in case of '10\0'
   int count = 0;
 
   while (card_name[0] != 'X') {
+    // Keep the loop going until the user stops the program with 'X'
+
+    // ask user for the card name
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
-    int val = 0;
-    switch (card_name[0]) {
-      case 'K': case 'Q': case 'J': val = 10; break;
-      case 'A': val = 11; break;
-      case 'X': val = -1;
-      default:
-      val = atoi(card_name);
-      if ((val < 2) || (val > 10)) {
-        puts("I don't understand that value!");
-        val = -1;
-      }
-    }
-    if (val == -1) {
+
+    // use the card_name to get the value of the card
+    // val = 0 means invalid card
+    int val = getVal(card_name);
+    if (val == 0) {
       continue;
     }
+
+    // update count based on the last card, print value of count
     count = updateCount(val, count);
     printf("Current count: %i\n", count);
   }
@@ -39,10 +37,28 @@ int main() {
 int updateCount(int val, int count) {
   // updateCount updates the count based on the value of val and a list of Rules
   // Rules: 2-6 = +1, 7-9 = 0, 10-Ace = -1
-  if ((val >= 2) && (val < 7)) {
+  if ((val >= 2) && (val <= 6)) {
     count++;
   } else if ((val == 10) || (val == 11)) {
     count--;
   }
   return count;
+}
+
+int getVal(char card_name[]) {
+  // Returns the val casted from card_name
+  // Returns 0 if invalid card_name
+  int val = 0;
+  switch (card_name[0]) {
+    case 'K': case 'Q': case 'J': val = 10; break;
+    case 'A': val = 11; break;
+    case 'X': val = 0; // val is already zero, but this is explicit
+    default:
+    val = atoi(card_name);
+    if ((val < 2) || (val > 10)) {
+      puts("I don't understand that value!");
+      val = 0;
+    }
+  }
+  return val;
 }
