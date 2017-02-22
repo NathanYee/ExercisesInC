1339fcc7bd0dc2857a78a56bbd9973c35f7906fb
nathanyee <nathaniel.yee@students.olin.edu>
2017-02-21 22:39:49 -0500

    added exercise 02

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -19,30 +19,50 @@ int *foo() {
   for (i=0; i<SIZE; i++) {
     array[i] = 42;
   }
-  return array;
+  return array; /*
+                 *  This returns a pointer to an array local to this function.
+                 *  As a result, the memory allocated for the array is closed
+                 *  and we will not be able to access it in main.
+                 */
 }
 
 void bar() {
   int i;
+  /* Create a new local array in bar */
   int array[SIZE];
 
   printf("%p\n", array);
 
+  /* Populate local array with numbers 0-4 */
   for (i=0; i<SIZE; i++) {
     array[i] = i;
+  }
+
+  /* Print the values of array to see that they exist */
+  for (i=0; i<SIZE; i++) {
+    printf("%d\n", array[i]);
   }
 }
 
 int main()
 {
   int i;
+  printf("Before foo()\n");
   int *array = foo();
+  printf("After foo()\n");
+
   printf("%i\n", sizeof(array));
+
+  printf("Before bar()\n");
   bar();
+  printf("After bar()\n");
+
 
   for (i=0; i<SIZE; i++) {
     printf("%d\n", array[i]);
   }
+  printf("After for loop main\n");
+
 
   return 0;
 }
