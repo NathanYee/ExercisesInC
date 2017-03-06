3c91aa1362fd46490030ffdcc1641d35f933d383
nathanyee <nathaniel.yee@students.olin.edu>
2017-03-06 12:19:54 -0500

    fixed twos complement

--- reading_questions/c_solutions/q5.2.c
+++ reading_questions/c_solutions/q5.2.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <limits.h>
 
+//assumes little endian
+void printBits(size_t const size, void const * const ptr)
+{
+  unsigned char *b = (unsigned char*) ptr;
+  unsigned char byte;
+  int i, j;
+
+  for (i=size-1;i>=0;i--)
+  {
+    for (j=7;j>=0;j--)
+    {
+      byte = (b[i] >> j) & 1;
+      printf("%u", byte);
+    }
+  }
+  puts("");
+}
+
 int main(){
 
   int before = 12;
   printf("%d\n", before);
 
-  int after = (before ^ INT_MIN) + 1;
+  int after = (before ^ -1) + 1;
   printf("%d\n", after);
+
+  int min = -1;
+  printBits(sizeof(min), &min);
 
   return 0;
 }
