c3d0324a5ff616fd2c4d21e9f4e95d880cb30f6d
nathanyee <nathaniel.yee@students.olin.edu>
2017-04-10 22:41:44 -0400

    added word_count program

---
+++ exercises/ex08/word_count.c
@@ -0,0 +1,15 @@
+/* Word counter using Glib library
+ * Nathan Yee
+ * Software Systems
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+int main(int argc, char** argv) {
+    GList* list = NULL;
+    list = g_list_append(list, "Hello world!");
+    printf("The first item is '%s'\n", (char *) g_list_first(list)->data);
+    return 0;
+}
