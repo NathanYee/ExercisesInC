229bfba477bb110b44dce76f86dee32ec37e872a
nathanyee <nathaniel.yee@students.olin.edu>
2017-04-10 23:02:51 -0400

    splitting file into space separated

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -1,15 +1,25 @@
 /* Word counter using Glib library
- * Nathan Yee
- * Software Systems
- */
+* Nathan Yee
+* Software Systems
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <glib.h>
+#include <glib/gstdio.h> // file utilites
 
 int main(int argc, char** argv) {
-    GList* list = NULL;
-    list = g_list_append(list, "Hello world!");
-    printf("The first item is '%s'\n", (char *) g_list_first(list)->data);
+    gssize length;
+    gchar *content, *filename = "laIncognita.txt";
+    if (g_file_get_contents (filename, &content, &length, NULL)) {
+        // Process contents
+        gchar** split = g_strsplit(content, " ", -1);
+        g_free (content);
+    }
+
+    //GList* list = NULL;
+    //list = g_list_append(list, "Hello world!");
+    //printf("The first item is '%s'\n", (char *) g_list_first(list)->data);
     return 0;
 }
